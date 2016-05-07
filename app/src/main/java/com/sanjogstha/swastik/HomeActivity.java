cf6b73755eace2a9a907dcda8ba19bfa881bf2d7
isanjog <sanjog.innovisto@gmail.com>
2016-05-07 10:40:22 +0545

    layout added

--- app/src/main/java/com/sanjogstha/swastik/HomeActivity.java
+++ app/src/main/java/com/sanjogstha/swastik/HomeActivity.java
@@ -2,6 +2,8 @@ package com.sanjogstha.swastik;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -12,14 +14,17 @@ public class HomeActivity extends AppCompatActivity{
 
     private TextView userNameTextView;
     String userName = null;
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        mDrawerLayout  = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mNavigationView =(NavigationView) findViewById(R.id.navigation);
         userNameTextView = (TextView) findViewById(R.id.userNameTextView);
-
         Intent getValue = getIntent();
         userName = getValue.getStringExtra("username");
-        userNameTextView.setText(userName);
+        //userNameTextView.setText(userName);
     }
 }

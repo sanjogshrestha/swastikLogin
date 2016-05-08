e55468c1eca42d5c5e87791a1f843cb8f393ef9f
isanjog <sanjog.innovisto@gmail.com>
2016-05-08 10:26:53 +0545

    toolbar defined

--- app/src/main/java/com/sanjogstha/swastik/HomeActivity.java
+++ app/src/main/java/com/sanjogstha/swastik/HomeActivity.java
@@ -1,30 +1,48 @@
 package com.sanjogstha.swastik;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 /**
  * Created by SanjogStha on 5/1/2016.
  */
-public class HomeActivity extends AppCompatActivity{
+public class HomeActivity extends AppCompatActivity implements View.OnClickListener, NavigationView.OnNavigationItemSelectedListener {
 
-    private TextView userNameTextView;
-    String userName = null;
     private DrawerLayout mDrawerLayout;
     private NavigationView mNavigationView;
+    private Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         mDrawerLayout  = (DrawerLayout) findViewById(R.id.drawerLayout);
         mNavigationView =(NavigationView) findViewById(R.id.navigation);
-        userNameTextView = (TextView) findViewById(R.id.userNameTextView);
-        Intent getValue = getIntent();
-        userName = getValue.getStringExtra("username");
-        //userNameTextView.setText(userName);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mNavigationView.setNavigationItemSelectedListener(this);
+
+        if (mToolbar != null) {
+            setSupportActionBar(mToolbar);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            mToolbar.setNavigationIcon(android.R.drawable.ic_input_add);
+            mToolbar.setNavigationOnClickListener(this);
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        mDrawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        return false;
     }
 }

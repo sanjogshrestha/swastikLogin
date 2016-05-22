48f4094a809eca9a36a591aab8e2c20f025c59a1
isanjog <sanjog.innovisto@gmail.com>
2016-05-22 09:12:04 +0545

    layout redefined for gallery item

--- app/src/main/java/com/sanjogstha/swastik/adapter/ImageAdapter.java
+++ app/src/main/java/com/sanjogstha/swastik/adapter/ImageAdapter.java
@@ -1,13 +1,15 @@
 package com.sanjogstha.swastik.adapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
-import android.widget.GridView;
 import android.widget.ImageView;
 
+import com.sanjogstha.swastik.Constant;
 import com.sanjogstha.swastik.GalleryActivity;
+import com.sanjogstha.swastik.R;
 import com.squareup.picasso.Picasso;
 
 /**
@@ -15,14 +17,18 @@ import com.squareup.picasso.Picasso;
  */
 public class ImageAdapter extends BaseAdapter {
 
-    String[] imageList = {"DSC_2390.jpg", "DSC_2386.jpg", "DSC_2383.jpg"};
-
-    String urlPath = "http://swastikcollege.edu.np/wp-content/gallery/students-in-lab/";
-
     Context context;
+
+    LayoutInflater layoutInflater;
+
+    ImageView galleryImageView;
+    private String[] imageList;
+    private String urlPath;
 
     public ImageAdapter(GalleryActivity galleryActivity) {
         context = galleryActivity;
+        imageList = Constant.imageList;
+        urlPath = Constant.urlPath;
     }
 
     @Override
@@ -42,11 +48,22 @@ public class ImageAdapter extends BaseAdapter {
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        ImageView imageView = new ImageView(context);
+        //ImageView imageView = new ImageView(context);
+        if(layoutInflater == null){
+            layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        }
+
+        if(view == null){
+            view = layoutInflater.inflate(R.layout.gallery_gridview_items, null);
+        }
+
+        galleryImageView = (ImageView) view.findViewById(R.id.gridImageView);
+
         Picasso.with(context)
                 .load(urlPath + imageList[i])
-                .into(imageView);
-        imageView.setLayoutParams(new GridView.LayoutParams(240,240));
-        return imageView;
+                .fit()
+                .into(galleryImageView);
+
+        return galleryImageView;
     }
 }

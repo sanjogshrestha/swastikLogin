9c55e0055208dd66f176495875562411a2b1534a
isanjog <sanjog.innovisto@gmail.com>
2016-05-22 09:10:41 +0545

    initialized reusable items

---
+++ app/src/main/java/com/sanjogstha/swastik/Constant.java
@@ -0,0 +1,12 @@
+package com.sanjogstha.swastik;
+
+/**
+ * Created by SanjogStha on 5/21/2016.
+ */
+public class Constant {
+
+    public static String[] imageList = {"DSC_2425.jpg", "DSC_2431.jpgsdas", "DSC_2441.jpgdasdas", "DSC_2447.jpg", "DSC_2451.jpg", "DSC_2452.jpg", "DSC_2455.jpg", "DSC_2450.JPG"};
+
+    public static String urlPath = "http://swastikcollege.edu.np/wp-content/gallery/outdoor/";
+
+}

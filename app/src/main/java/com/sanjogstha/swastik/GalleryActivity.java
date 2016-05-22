49c624ee5539b14552df4cf7b40b5703d0505f9b
isanjog <sanjog.innovisto@gmail.com>
2016-05-22 09:12:26 +0545

    modified

--- app/src/main/java/com/sanjogstha/swastik/GalleryActivity.java
+++ app/src/main/java/com/sanjogstha/swastik/GalleryActivity.java
@@ -1,9 +1,12 @@
 package com.sanjogstha.swastik;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.sanjogstha.swastik.adapter.ImageAdapter;
@@ -25,12 +28,25 @@ public class GalleryActivity extends AppCompatActivity{
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         galleryGridView = (GridView) findViewById(R.id.galleryGridView);
 
+        galleryGridView.setAdapter(new ImageAdapter(this));
+
         if (toolbar != null) {
             setSupportActionBar(toolbar);
             toolbar.setTitle(R.string.gallery);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        galleryGridView.setAdapter(new ImageAdapter(this));
+        galleryGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Intent galleryItem = new Intent(GalleryActivity.this, GalleryItemActivity.class);
+                galleryItem.putExtra("position", i);
+                startActivity(galleryItem);
+
+            }
+        });
+
+
     }
 }
